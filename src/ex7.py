d05249cbd3b02b830f2ce2536f3da8d3dfe263f3
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2019-12-10 15:02:56 +0100

    exo 7

---
+++ src/ex7.py
@@ -0,0 +1,29 @@
+"""
+7
+"""
+n2ch = "".join([
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "abcdefghijklmnopqrstuvwxyz",
+    "0123456789-_",
+])
+ch2n = dict(zip(n2ch, range(len(n2ch))))
+# print(ch2n)
+
+
+def depart(int_list) -> list:
+
+    depart = []
+    for integer in int_list:
+        depart.append(
+            list(ch2n.keys())[list(ch2n.values()).index(integer)])
+    print(depart)
+
+
+depart([16, 20, 9, 3, 17, 0])
+
+
+# depart = [16, 20, 9, 3, 17, 0]
+# for integer in depart:
+#     depart.append(
+#         list(ch2n.keys())[list(ch2n.values()).index(integer)])
+#     print(depart)
